56f1ee9c714380e0aa315d29460ff48b916ad059
dongyangyang <15000814726@163.com>
2018-06-28 18:17:32 +0800

    PerformanceAspect面向切面编程实现接口性能监控，AOP的支持

---
+++ src/main/java/com/springboot/common/aspect/PerformanceAspect.java
@@ -0,0 +1,41 @@
+package com.springboot.common.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * Aspect接口性能监控
+ */
+@Component
+@Aspect
+@SuppressWarnings("all")
+public class PerformanceAspect {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PerformanceAspect.class);
+
+    @Before("execution(* com.springboot.controller.*.*(..))")
+    public void before(){
+//		System.out.println("方法执行前");
+    }
+
+    @After("execution(* com.springboot.controller.*.*(..))")
+    public void after(){
+//		System.out.println("方法执行后");
+    }
+
+    @Around("execution(* com.springboot.controller.*.*(..))")
+    public Object around(ProceedingJoinPoint jp) throws Throwable{
+        Long start = System.currentTimeMillis();
+        Object result = jp.proceed();
+        Long end = System.currentTimeMillis();
+        LOGGER.info(jp.getSignature() +" consumption time : "+ (end-start) + "ms");
+        return result;
+    }
+
+}
